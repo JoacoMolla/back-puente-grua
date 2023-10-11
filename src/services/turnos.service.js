4cbeb82360dc891266ea985114ea622e2376240d
90019-Molla-Joaquin <90019@sistemas.frc.utn.edu.ar>
2023-10-11 11:44:06 -0300

    fecha arreglada

--- src/services/turnos.service.js
+++ src/services/turnos.service.js
@@ -2,18 +2,23 @@ const db = require("../models/database.js");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
+const transformarFechaACadena = (fecha) => {
+    return `${fecha.toLocaleDateString()}` + ' ' +
+    `${fecha.toLocaleTimeString()}`
+}
+
 const getAll = async () => {
     const resultado = await db.models.turnos.findAll({
         attributes: [
             'idTurno',
             'fechaHoraCreacion',
-            'idDetalleTurno',
-            'idEstadoTurno',
             'usuarioLegajo',
             'adminLegajo'
         ],
         order: [['idTurno', 'ASC']],
+
         include: [
+            // Detalle turno y sus pk y fk
             {
                 model: db.models.detalleturno,
                 as: 'detalleturno',
@@ -25,18 +30,47 @@ const getAll = async () => {
                         attributes: ['idTurnoAtrasado', 'esAtrasado', 'minutosAtrasado']
                     }
                 ]
+            },
+            // Estado del turno
+            {
+                model: db.models.estadoturno,
+                as: 'estadoturno',
+                attributes: ['idEstadoTurno', 'descripcion'],
+                include: [
+                    {
+                        model: db.models.nombreestadoturno,
+                        as: 'nombreestadoturno',
+                        attributes: ['idNombreEstadoTurno', 'nombre']
+                    }
+                ]
             }
         ]
     });
     return resultado.map((t) => {
         return {
-            idTurno: t.dataValues.idTurno,
-            fechaHoraCreacion: t.dataValues.fechaHoraCreacion,
-            idDetalleTurno: t.dataValues.idDetalleTurno,
-            idEstadoTurno: t.dataValues.idEstadoTurno,
-            usuarioLegajo: t.dataValues.usuarioLegajo,
-            adminLegajo: t.dataValues.adminLegajo,
-            detalleturno: t.dataValues.detalleturno
+            turno: {
+                idTurno: t.dataValues.idTurno,
+                fechaHoraCreacion: new Date(t.dataValues.fechaHoraCreacion).toString(),
+                usuarioLegajo: t.dataValues.usuarioLegajo,
+                adminLegajo: t.dataValues.adminLegajo
+            },
+            detalleTurno: {
+                idDetalleTurno: t.dataValues.idDetalleTurno,
+                diaHoraInicio: transformarFechaACadena(new Date(t.dataValues.detalleturno.diaHoraInicio)),
+                idZonaInicio: t.dataValues.detalleturno.idZonaInicio
+            },
+            turnoAtrasado: {
+                esAtrasado: t.dataValues.detalleturno.turnoatrasado.esAtrasado,
+                minutosAtrasado: t.dataValues.detalleturno.turnoatrasado.minutosAtrasado,
+            },
+            estadoTurno: {
+                idEstadoTurno: t.dataValues.estadoturno.idEstadoTurno,
+                descripcion: t.dataValues.estadoturno.descripcion,
+            },
+            nombreEstadoTurno: {
+                idNombreEstadoTurno: t.dataValues.estadoturno.nombreestadoturno.idNombreEstadoTurno,
+                nombre: t.dataValues.estadoturno.nombreestadoturno.nombre
+            },
         }
     });
 }
